88be8dc62c51bbe0e5803ac8adc69a04018b9218
Subash Shanmugam <SubashS0411>
2026-04-07 11:01:45 +0530

    Solved the leetcode problem 55

---
+++ CoreDsa/GreedyApproach/src/JumpGame.java
@@ -0,0 +1,22 @@
+public class JumpGame {
+//    Solved the leetcode problem:55.Jump Game
+//    https://leetcode.com/problems/jump-game/description/
+    public static void main(String[] args) {
+        int[] arr={2, 3, 1, 1, 4};
+        System.out.println(CanJump(arr));
+    }
+    public static boolean CanJump(int[] arr){
+        int maxreach=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(i>maxreach){
+                return  false;
+            }
+            maxreach=Math.max(maxreach,i+arr[i]);
+            if(maxreach>=arr.length-1){
+                return true;
+            }
+        }
+        return true;
+    }
+
+}
